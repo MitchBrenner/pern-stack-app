79c63580f6d31ae0aab97b1b2a067852e938d09a
Mitchell Brenner <mitchbrenner8@gmail.com>
2025-06-30 16:42:05 -0600

    Created controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,100 @@
-export const getAllProducts = async (req, res) => {
-  // Logic to get all products
+import { sql } from "../config/db.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await sql`
+            SELECT * FROM products
+            ORDER BY created_at DESC
+            `;
+    console.log("Products fetched: ", products);
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.error("Error fetching products: ", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 };
 
 export const createProduct = async (req, res) => {
-  // Logic to create a new product
+  const { name, price, image } = req.body;
+
+  if (!name || !price || !image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+
+  try {
+    const newProduct = await sql`
+      INSERT INTO products (name, price, image)
+      VALUES (${name}, ${price}, ${image})
+      RETURNING *
+    `;
+
+    console.log("Product created: ", newProduct);
+    res.status(201).json({ success: true, data: newProduct[0] });
+  } catch (error) {
+    console.error("Error creating product: ", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const getProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await sql`
+      SELECT * FROM products WHERE id = ${id}
+    `;
+
+    res.status(200).json({ success: true, data: product[0] });
+  } catch (error) {
+    console.error("Error fetching product: ", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, price, image } = req.body;
+
+  try {
+    const updatedProduct = await sql`
+      UPDATE products
+      SET name = ${name}, price = ${price}, image = ${image}
+      WHERE id = ${id}
+      RETURNING *
+    `;
+
+    if (updatedProduct.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+    res.status(200).json({ success: true, data: updatedProduct[0] });
+  } catch (error) {
+    console.error("Error updating product: ", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedProduct = await sql`
+            DELETE FROM products WHERE id = ${id} RETURNING *
+        `;
+
+    if (deletedProduct.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
+
+    console.log("Product deleted: ", deletedProduct[0]);
+    res.status(200).json({ success: true, data: deletedProduct[0] });
+  } catch (error) {
+    console.error("Error deleting product: ", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 };
