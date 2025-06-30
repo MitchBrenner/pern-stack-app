8e393d466d2200f60d92d36bb752c888f961b132
Mitchell Brenner <mitchbrenner8@gmail.com>
2025-06-30 14:29:24 -0600

    Initialized express server

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cors()); // Enable CORS for all routes
@@ -15,6 +19,6 @@ app.get("/test", (req, res) => {
   res.send("Hello from backend");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
